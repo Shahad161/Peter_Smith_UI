ccfec4c646065640d2c22911bc333786fe4d7155
Shahad A.Hamza <Shahad.Hamza.014@gmail.com>
2021-04-28 23:21:12 +0300

    Peter Smith UI

---
+++ app/src/main/java/com/example/petersmith/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.petersmith
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
